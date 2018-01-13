cbde30b812d9a542929418771b623b7cc01258e2
Breenbo <berrehuelbruno@gmail.com>
2018-01-12 22:03:14 -0400

    js - create city table

---
+++ 6.javascript/homework/myproject/script.sql
@@ -0,0 +1,14 @@
+CREATE TABLE places (
+    country_code CHAR(2),
+    postal_code VARCHAR(20),
+    place_name VARCHAR(180),
+    admin_name1 VARCHAR(100),
+    admin_code1 VARCHAR(20),
+    admin_name2 VARCHAR(100),
+    admin_code2 VARCHAR(20),
+    admin_name3 VARCHAR(100),
+    admin_code3 VARCHAR(20),
+    latitude FLOAT,
+    longitude FLOAT,
+    accuracy INTEGER(1)
+)
